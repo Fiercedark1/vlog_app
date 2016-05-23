6f03a5a5a296ee9a47e14f23aec3a115e15b02e4
Fiercedark1 <ecarra7979@gmail.com>
2016-05-23 01:56:59 -0400

    Feature update: Blog posts:: Ability to add new, css, two tests posts, changed icons in application_template

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508114307) do
+ActiveRecord::Schema.define(version: 20160523050446) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "wistia"

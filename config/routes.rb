519fb9b43f1c083978201c3f8d4c38c3a62e4ee9
Fiercedark1 <ecarra7979@gmail.com>
2016-05-23 06:53:28 -0400

    chages to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  devise_for :users do
+    delete 'logout' => 'sessions#destroy', :as => :destroy_user_session
+    get 'login' => 'devise/sessions#new'
+  end
   get 'pages/about'
 
   get 'pages/contact'

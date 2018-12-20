2738a364ab7d55fea64050ea1ad69e9e9d7d9766
Ubuntu <ubuntu@ip-172-25-125-154.ec2.internal>
2018-12-20 02:15:39 +0000

    ruby app update with Datadog

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,22 +1,11 @@
 # config/initializers/datadog-tracer.rb
-# f = File.new("log/development.log", "w+")
 require 'ddtrace'
-
-tracer = Datadog.tracer
-sampler = Datadog.configure do |c|
+f = File.new("log/development.log", "w+")
+sampler = Datadog::RateSampler.new(1)
+Datadog.configure do |c|
   c.tracer hostname: 'trace-vlog.local'
-  c.use :rails, service_name: 'vlog'
-  c.tracer sampler: Datadog::RateSampler.new(1)
-  c. tracer priority_sampling: Datadog::Ext::Priority::AUTO_KEEP
-  c.tracer tags: { 'env' => 'rubyrails' }
-#  c.tracer priority_sampling: true
-#  c.tracer log: Logger.new(f)
+  c.use :rails, service_name: 'ruby-vlog'
+  c.tracer priority_sampling: true
+  c.tracer tags: { 'env' => 'datad0g.com' }
+  c.tracer log: Logger.new(f)
 end
-#Datadog.configure do |c|
-#  c.use :rails, service_name: 'vlog'
-#end
-#Rails.configuration.datadog_trace = {
-#  auto_instrument: true,
-#  auto_instrument_redis: true,
-#  default_service: 'vlog'
-#}
